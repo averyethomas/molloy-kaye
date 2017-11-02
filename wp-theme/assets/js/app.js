2714aaed7c2fc07a8167c304d5255d68cbe4d115
Avery Thomas <averyethomas@gmail.com>
2017-11-01 22:27:13 -0400

    update gallery and rem

--- wp-theme/assets/js/app.js
+++ wp-theme/assets/js/app.js
@@ -4,3 +4,13 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
 
     
 }]);
+
+
+app.controller('galleryCtrl', ['$scope', function($scope){
+
+    $scope.lightbox = function(n){
+        $scope.gallery = document.getElementsByClassName('image');
+        
+        $scope.active = $scope.gallery[n];
+    }
+}])
