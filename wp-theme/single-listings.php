ae11301f6eaf0af1bfd1691535c9c6a04a5dfeaf
Avery Thomas <averyethomas@gmail.com>
2017-12-17 20:38:03 -0500

    download on form submit

--- wp-theme/single-listings.php
+++ wp-theme/single-listings.php
@@ -215,11 +215,13 @@
         </div>
     </div>
 </div>
+<a href="<?php the_field('offering_memorandum'); ?>" download id="download" hidden></a>
 <script>
     document.addEventListener( 'wpcf7mailsent', function( event ) {
         console.log("<?php the_field('offering_memorandum'); ?>");
+        //window.open("<?php the_field('offering_memorandum'); ?>","_blank");
+        document.getElementById('download').click();
     }, false );    
-    //    location = '<?php the_field('offering_memorandum'); ?>
 </script>
 <?php
 
